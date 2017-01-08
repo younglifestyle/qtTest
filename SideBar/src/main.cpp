617477316f58e371840da11223cfe0fc71590814
younglifestyle <718800822@qq.com>
2017-01-08 23:23:31 +0800

    myTest branchgit commit -a!

--- SideBar/src/main.cpp
+++ SideBar/src/main.cpp
@@ -7,9 +7,9 @@ int main(int argc, char *argv[])
     Widget w;
 
     /* 全屏显示，另外一种全屏显示w.showFullScreen();，效果连标题栏都能去掉 */
-    w.setMinimumSize(1510, 850);        /* 设置窗口最小尺寸，在Designer中无法修改尺寸了，用代码强行修改 */
-    w.resize(1510, 800);                /* 设置完尺寸后，重新显示窗口的尺寸 */
-    w.setWindowState(Qt::WindowMaximized);    /* 全屏显示 */
+    w.setMinimumSize(960, 630);        /* 设置窗口最小尺寸，在Designer中无法修改尺寸了，用代码强行修改 */
+    w.resize(960, 630);                /* 设置完尺寸后，重新显示窗口的尺寸 */
+//    w.setWindowState(Qt::WindowMaximized);    /* 全屏显示 */
     w.show();
 
     return a.exec();
